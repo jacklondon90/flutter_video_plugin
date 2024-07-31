97a54a9f8415f156f2cd5ea04b6bca31f1696177
Djasur Foteev <jfoteev@gmail.com>
2024-07-31 14:27:42 +0500

    trying to launch android

--- android/src/main/kotlin/uz/frame/ExoPlayerViewFactory.kt
+++ android/src/main/kotlin/uz/frame/ExoPlayerViewFactory.kt
@@ -1,10 +1,11 @@
+// android/src/main/kotlin/uz/frame/player/ExoPlayerViewFactory.kt
 package uz.frame.player
 
 import android.content.Context
 import io.flutter.plugin.common.BinaryMessenger
-import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
+import io.flutter.plugin.common.StandardMessageCodec
 
 class ExoPlayerViewFactory(private val messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
     override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
