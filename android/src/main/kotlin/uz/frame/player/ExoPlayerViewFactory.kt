5d756eb201b1743c4570df5716c23858d2eddb98
Djasur Foteev <jfoteev@gmail.com>
2024-08-05 21:56:13 +0500

    fixing android

---
+++ android/src/main/kotlin/uz/frame/player/ExoPlayerViewFactory.kt
@@ -0,0 +1,14 @@
+// android/src/main/kotlin/uz/frame/player/ExoPlayerViewFactory.kt
+package uz.frame.player
+
+import android.content.Context
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+import io.flutter.plugin.common.StandardMessageCodec
+
+class ExoPlayerViewFactory(private val messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        return ExoPlayerView(context, viewId, messenger)
+    }
+}
