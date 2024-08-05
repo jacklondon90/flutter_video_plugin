fd52ec7f6ff2ba4c849658576b366a5f5efb1be6
Djasur Foteev <jfoteev@gmail.com>
2024-08-06 00:19:22 +0500

    resolving package name:

--- example/android/app/src/main/kotlin/com/example/flutter_video_plugin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_video_plugin_example/MainActivity.kt
@@ -2,12 +2,15 @@ package uz.frame.player
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.platform.PlatformViewRegistry
 
 class MainActivity : FlutterActivity() {
-
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        flutterEngine.platformViewsController.registry.registerViewFactory("PlayerPlugin", ExoPlayerViewFactory(flutterEngine.dartExecutor.binaryMessenger))
+        val registry: PlatformViewRegistry = flutterEngine.platformViewsController.registry
+        val messenger: BinaryMessenger = flutterEngine.dartExecutor.binaryMessenger
+        registry.registerViewFactory("PlayerView", PlayerViewFactory(messenger))
     }
 }
--- example/android/app/src/main/kotlin/com/example/flutter_video_plugin_example/PlayerViewFactory.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_video_plugin_example/PlayerViewFactory.kt
@@ -1,14 +1,15 @@
 package uz.frame.player
 
 import android.content.Context
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
 import io.flutter.plugin.common.BinaryMessenger
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 import io.flutter.plugin.common.StandardMessageCodec
 
-class ExoPlayerViewFactory(private val messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
-    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+class PlayerViewFactory(private val messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    @OptIn(UnstableApi::class) override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
         return PlayerPlugin(context, viewId, messenger)
     }
 }
