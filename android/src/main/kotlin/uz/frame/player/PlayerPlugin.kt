ea5fe74d33811b8a58f3628c3f91ee6e694d494f
Djasur Foteev <jfoteev@gmail.com>
2024-08-06 11:10:06 +0500

    fixes

--- android/src/main/kotlin/uz/frame/player/PlayerPlugin.kt
+++ android/src/main/kotlin/uz/frame/player/PlayerPlugin.kt
@@ -1,31 +1,18 @@
 package uz.frame.player
 
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
 /** PlayerPlugin */
-class PlayerPlugin: FlutterPlugin, MethodCallHandler {
-  private lateinit var channel : MethodChannel
-
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "player")
-    channel.setMethodCallHandler(this)
+class PlayerPlugin: FlutterPlugin {
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    flutterPluginBinding
+      .platformViewRegistry
+      .registerViewFactory("exoplayer_view", ExoPlayerViewFactory(flutterPluginBinding.binaryMessenger))
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
-  }
-
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    // Clean up resources if needed
   }
 }
