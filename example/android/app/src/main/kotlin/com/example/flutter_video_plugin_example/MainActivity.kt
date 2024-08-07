2405a0bdc3a2abb732aa5da572b106c63c9d23f1
Djasur Foteev <jfoteev@gmail.com>
2024-08-07 09:57:35 +0500

    fixes

--- example/android/app/src/main/kotlin/com/example/flutter_video_plugin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_video_plugin_example/MainActivity.kt
@@ -2,12 +2,13 @@ package uz.frame.player
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        flutterEngine.platformViewsController.registry.registerViewFactory("PlayerPlugin", PlayerViewFactory(flutterEngine.dartExecutor.binaryMessenger))
+        flutterEngine.platformViewsController.registry.registerViewFactory(
+            "PlayerPlugin",
+            PlayerViewFactory(flutterEngine.dartExecutor.binaryMessenger)
+        )
     }
 }
