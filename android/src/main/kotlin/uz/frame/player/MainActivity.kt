e9b7401446f3484591a1e917d4094a6aaa6e9fa4
Djasur Foteev <jfoteev@gmail.com>
2024-08-07 21:52:30 +0500

    fixes

---
+++ android/src/main/kotlin/uz/frame/player/MainActivity.kt
@@ -0,0 +1,15 @@
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        println("Registering PlayerViewFactory")
+        flutterEngine.platformViewsController.registry.registerViewFactory(
+            "PlayerPlugin",
+            PlayerViewFactory(flutterEngine.dartExecutor.binaryMessenger)
+        )
+    }
+}
