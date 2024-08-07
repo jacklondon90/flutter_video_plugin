adff4ac18728358b740143d04f4eac91de650a84
Djasur Foteev <jfoteev@gmail.com>
2024-08-07 11:12:37 +0500

    fixes

--- android/src/main/kotlin/uz/frame/player/PlayerPlugin.kt
+++ android/src/main/kotlin/uz/frame/player/PlayerPlugin.kt
@@ -1,29 +1,27 @@
 package uz.frame.player
 
-import android.content.Context
 import androidx.annotation.NonNull
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.platform.PlatformViewFactory
 
 /** PlayerPlugin */
 class PlayerPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "PlayerPlugin")
     channel.setMethodCallHandler(this)
-
-    // Register the platform view
-    flutterPluginBinding
-        .platformViewRegistry
-        .registerViewFactory("PlayerPlugin", PlayerViewFactory(flutterPluginBinding.binaryMessenger))
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
@@ -31,7 +29,7 @@ class PlayerPlugin: FlutterPlugin, MethodCallHandler {
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
 }
