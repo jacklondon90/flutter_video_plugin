4eb5647bfe3abb5cc1343903756383fa9c441bf3
Djasur Foteev <jfoteev@gmail.com>
2024-08-07 23:06:42 +0500

    fixes

--- test/flutter_video_plugin_method_channel_test.dart
+++ test/flutter_video_plugin_method_channel_test.dart
@@ -6,10 +6,11 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   MethodChannelFlutterVideoPlugin platform = MethodChannelFlutterVideoPlugin();
-  const MethodChannel channel = MethodChannel('flutter_video_plugin');
+  const MethodChannel channel = MethodChannel('PlayerPlugin');
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
       channel,
       (MethodCall methodCall) async {
         return '42';
@@ -18,7 +19,8 @@ void main() {
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
