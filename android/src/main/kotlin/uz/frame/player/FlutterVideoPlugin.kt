29d0f580eedd477c23d752eb273265bd1946bc5f
Djasur Foteev <jfoteev@gmail.com>
2024-08-08 13:37:22 +0500

    fixes

--- android/src/main/kotlin/uz/frame/player/FlutterVideoPlugin.kt
+++ android/src/main/kotlin/uz/frame/player/FlutterVideoPlugin.kt
@@ -2,27 +2,29 @@ package uz.frame.player
 
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 
 /** FlutterVideoPlugin */
-class FlutterVideoPlugin: FlutterPlugin, ActivityAware {
+class FlutterVideoPlugin: FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
+
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_video_plugin")
+        channel.setMethodCallHandler(this)
         flutterPluginBinding.platformViewRegistry.registerViewFactory("exoplayer_view", ExoPlayerViewFactory(flutterPluginBinding.binaryMessenger))
     }
 
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
+
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    }
-
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    }
-
-    override fun onDetachedFromActivity() {
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    }
-
-    override fun onDetachedFromActivityForConfigChanges() {
+        channel.setMethodCallHandler(null)
     }
 }
