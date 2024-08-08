cba05657f784fc43dcf8b1ce080a719163b72c2b
Djasur Foteev <jfoteev@gmail.com>
2024-08-08 19:20:11 +0500

    fixes

---
+++ android/app/src/main/kotlin/uz/frame/player/FlutterVideoPlugin.kt
@@ -0,0 +1,30 @@
+package uz.frame.player
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+
+/** FlutterVideoPlugin */
+class FlutterVideoPlugin: FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_video_plugin")
+        channel.setMethodCallHandler(this)
+        flutterPluginBinding.platformViewRegistry.registerViewFactory("exoplayer_view", ExoPlayerViewFactory(flutterPluginBinding.binaryMessenger))
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+}
