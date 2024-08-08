1d38998cb854e90142d9880d809264cfb81088fd
Djasur Foteev <jfoteev@gmail.com>
2024-08-08 19:15:03 +0500

    fixed example folder

---
+++ android/app/src/main/kotlin/com/example/flutter_video_plugin_example/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.flutter_video_plugin_example
+import PlayerViewFactory
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        println("Registering PlayerViewFactory")
+        flutterEngine.platformViewsController.registry.registerViewFactory(
+            "PlayerPlugin",
+            PlayerViewFactory(flutterEngine.dartExecutor.binaryMessenger)
+        )
+    }
+}
