2fa7b6240224518bbb90e9a038075b06b1cf0bec
Djasur Foteev <jfoteev@gmail.com>
2024-08-08 22:08:03 +0500

    fixes

---
+++ android/src/main/kotlin/uz/frame/player/PlayerViewFactory.kt
@@ -0,0 +1,17 @@
+package uz.frame.player
+
+import android.content.Context
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+
+class PlayerViewFactory(private val messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    @OptIn(UnstableApi::class)
+    override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
+        println("Creating PlayerPlugin with viewId: $viewId")
+        return PlayerPlugin(context!!, viewId, messenger)
+    }
+}
