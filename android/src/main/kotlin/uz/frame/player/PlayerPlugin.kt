6544fc484a0dccca9a083ef1e362cfa5c60ac677
Djasur Foteev <jfoteev@gmail.com>
2024-08-09 12:42:15 +0500

    debugging

--- android/src/main/kotlin/uz/frame/player/PlayerPlugin.kt
+++ android/src/main/kotlin/uz/frame/player/PlayerPlugin.kt
@@ -9,25 +9,9 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 class PlayerPlugin : FlutterPlugin, MethodCallHandler {
-    private lateinit var channel: MethodChannel
-
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        Log.d("PlayerPlugin", "onAttachedToEngine called in Main Project")
+        Log.d("PlayerPlugin", "onAttachedToEngine called")
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "uz.frame.player")
         channel.setMethodCallHandler(this)
-    }
-
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        Log.d("PlayerPlugin", "Method call received: ${call.method} in Main Project")
-        if (call.method == "getPlatformVersion") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")
-        } else {
-            result.notImplemented()
-        }
-    }
-
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        Log.d("PlayerPlugin", "onDetachedFromEngine called in Main Project")
-        channel.setMethodCallHandler(null)
     }
 }
